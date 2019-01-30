b3bda50c34862a08308ab9753177be60370fbe66
pyseed <pyseed.dev@gmail.com>
2019-01-30 17:25:11 +0100

    [ADD] console: sub command handler

--- dregcli/console.py
+++ dregcli/console.py
@@ -1,2 +1,36 @@
+import argparse
+
+
+class CommandHandler(object):
+    def run(self, args):
+        print(self.Meta.command)
+
+
+class RepositoriesCommandHandler(CommandHandler):
+    class Meta:
+        command = "reps"
+
+    def run(self, args):
+        super().run(args)
+
+
 def main():
-    raise NotImplementedError("WIP")
+    parser = argparse.ArgumentParser(
+        prog="dregcli",
+        description="Docker registry API v2 client",
+    )
+
+    subparsers = parser.add_subparsers(help='sub-commands')
+
+    subparser_repositories = subparsers.add_parser(
+        'reps', help='List repositories'
+    )
+    subparser_repositories.set_defaults(
+        func=lambda args: RepositoriesCommandHandler().run(args)
+    )
+
+    arguments = parser.parse_args()
+    if hasattr(arguments, 'func'):
+        arguments.func(arguments)
+    else:
+        parser.print_help()
