e9cc9fc50719f4e8bdbe935cdd47b70c827cf2cf
pyseed <pyseed.dev@gmail.com>
2019-03-26 16:11:55 +0100

    [IMP] delete command handler: --from-count use an index number based on layers (tags), integration test

--- dregcli/tests/tests_integration/tests_delete/test_delete_from_count.py
+++ dregcli/tests/tests_integration/tests_delete/test_delete_from_count.py
@@ -46,15 +46,22 @@ class TestDeleteFromCount:
         assert sorted(expected_tags_by_desc_date) == \
             sorted(fixture_delete_tags)
 
+        # remove from staging/master-*-1388, see fixture_delete_tags docstring
         from_count = 3
+        should_left = [
+            'master-b2a7d05ca36cdd3e8eb092f857580b3ed0f7159a-1386',
+            'prod',
+            'master-1c48755c0b257ccd106badcb973a36528f833fc0-1387',
+            'old-staging',
+        ]
+
         handler = DeleteCommandHandler()
         deleted = handler.run(fixture_registry_url, fixture_repository, False,
                               from_count=from_count)
 
         # check delete from 3rd
-        should_deleted = expected_tags_by_desc_date[from_count - 1:]
-        assert deleted == should_deleted
+        should_deleted = [tag for tag in repo_tags if tag not in should_left]
+        assert sorted(deleted) == sorted(should_deleted)
 
-        # check reop should have head of no deleted ones
-        should_left = expected_tags_by_desc_date[:from_count - 1]
+        # check repo should have head of no deleted ones
         assert sorted(repo.tags()) == sorted(should_left)
